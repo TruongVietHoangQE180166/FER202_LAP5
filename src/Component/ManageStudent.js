763c411c1aa0e0806f983a07d0ff621ed006ebe0
hacker tiktok <139045110+hoanglaptop6204@users.noreply.github.com>
2024-10-22 08:46:45 +0700

    update modal for add student

--- src/Component/ManageStudent.js
+++ src/Component/ManageStudent.js
@@ -9,17 +9,26 @@ function StudentManagement() {
     { id: 3, name: 'Lê Thị Minh', code: 'QE180045', birth: '07/07/2004', CCCD: '052204001337', active: true },
     { id: 4, name: 'Phạm Tuấn Anh', code: 'QE180112', birth: '02/09/2004', CCCD: '052204001338', active: false },
   ]);
-
+  //state for add
   const [name, setName] = useState('');
   const [code, setCode] = useState('');
   const [birth, setBirth] = useState('');
   const [CCCD, setCCCD] = useState('');
+  //state for select all
   const [selectedStudents, setSelectedStudents] = useState([]);
   const [selectAll, setSelectAll] = useState(false);
 
   // State for modal
-  const [showModal, setShowModal] = useState(false);
+  const [showAddModal, setShowAddModal] = useState(false);
+  const [showEditModal, setShowEditModal] = useState(false);
   const [editStudent, setEditStudent] = useState(null);
+
+  //state for search 
+  const [searchName, setSearchName] = useState('');
+  const [searchCode, setSearchCode] = useState('');
+  const [searchBirth, setSearchBirth] = useState('');
+  const [searchCCCD, setSearchCCCD] = useState('');
+
 
   // Thêm sinh viên mới
   function handleAddStudent() {
@@ -47,6 +56,16 @@ function StudentManagement() {
     setStudents(updatedStudents);
     setSelectedStudents(selectedStudents.filter(sId => sId !== id));
   }
+  // search sinh vien 
+  function handleSearch() {
+    const filteredStudents = students.filter(student =>
+      (searchName === '' || student.name.includes(searchName)) &&
+      (searchCode === '' || student.code.includes(searchCode)) &&
+      (searchBirth === '' || student.birth.includes(searchBirth)) &&
+      (searchCCCD === '' || student.CCCD.includes(searchCCCD))
+    );
+    setStudents(filteredStudents);
+  }
 
   // Thay đổi trạng thái Active
   function toggleStatus(id) {
@@ -56,10 +75,25 @@ function StudentManagement() {
     setStudents(updatedStudents);
   }
 
-  // Edit Student
+  // Mở modal thêm sinh viên
+  function handleOpenAddModal() {
+    setShowAddModal(true);
+  }
+
+  // Đóng modal thêm sinh viên
+  function handleCloseAddModal() {
+    setShowAddModal(false);
+  }
+
+  // Mở modal chỉnh sửa sinh viên
   function handleEditStudent(student) {
     setEditStudent(student);
-    setShowModal(true);
+    setShowEditModal(true);
+  }
+
+  // Đóng modal chỉnh sửa sinh viên
+  function handleCloseEditModal() {
+    setShowEditModal(false);
   }
 
   // Update student information
@@ -68,10 +102,10 @@ function StudentManagement() {
       student.id === editStudent.id ? editStudent : student
     );
     setStudents(updatedStudents);
-    setShowModal(false);
+    setShowEditModal(false);
   }
-// Cập nhật số lượng sinh viên được chọn
-function handleSelectStudent(id, isChecked) {
+  // Cập nhật số lượng sinh viên được chọn
+  function handleSelectStudent(id, isChecked) {
     if (isChecked) {
       setSelectedStudents([...selectedStudents, id]);
     } else {
@@ -89,125 +123,141 @@ function handleSelectStudent(id, isChecked) {
       setSelectedStudents([]);
     }
   }
-   // Đồng bộ trạng thái Select All với danh sách đã chọn
-   useEffect(() => {
-    if (selectedStudents.length === students.length && students.length > 0) {
-      setSelectAll(true);
-    } else {
-      setSelectAll(false);
-    }
+  // funtion for clear all 
+  function handleClearAll() {
+    setStudents([]); // Xóa toàn bộ danh sách sinh viên
+    setSelectedStudents([]); // Đảm bảo danh sách sinh viên đã chọn cũng được xóa
+    setSelectAll(false); // Bỏ chọn trạng thái "Select All" nếu có
+  }
+  
+  // Đồng bộ trạng thái Select All với danh sách đã chọn
+  useEffect(() => {
+    setSelectAll(selectedStudents.length === students.length && students.length > 0);
   }, [selectedStudents, students]);
   return (
     <div className="container mt-5">
-      <h3>Total Selected Student: {selectedStudents.length}</h3>
-      <Button variant="dark" onClick={() => setStudents([])} className="mb-3">
-        Clear
-      </Button>
-
+      <h3>StudentManagement</h3>
+      
+      <Button variant="dark" onClick={handleOpenAddModal} className="mb-3">
+        Add Student
+      </Button>{' '}
       <Form className="mb-3">
         <Row>
           <Col>
             <Form.Control
               type="text"
-              placeholder="Student Name"
-              value={name}
-              onChange={e => setName(e.target.value)}
+              placeholder="Search by Name"
+              value={searchName}
+              onChange={e => setSearchName(e.target.value)}
             />
           </Col>
           <Col>
             <Form.Control
               type="text"
-              placeholder="Student Code"
-              value={code}
-              onChange={e => setCode(e.target.value)}
+              placeholder="Search by Code"
+              value={searchCode}
+              onChange={e => setSearchCode(e.target.value)}
             />
           </Col>
           <Col>
             <Form.Control
               type="text"
-              placeholder="Student Birth"
-              value={birth}
-              onChange={e => setBirth(e.target.value)}
+              placeholder="Search by Birth Date"
+              value={searchBirth}
+              onChange={e => setSearchBirth(e.target.value)}
             />
           </Col>
           <Col>
             <Form.Control
               type="text"
-              placeholder="CCCD"
-              value={CCCD}
-              onChange={e => setCCCD(e.target.value)}
+              placeholder="Search by CCCD"
+              value={searchCCCD}
+              onChange={e => setSearchCCCD(e.target.value)}
             />
           </Col>
           <Col>
-            <Button variant="dark" onClick={handleAddStudent}>
-              Add
+            <Button variant="dark" onClick={handleSearch}>
+              Search
             </Button>
           </Col>
         </Row>
       </Form>
 
       <Table striped bordered hover>
-        <thead>
-          <tr>
-          <th>
-      <Form.Check
-        type="checkbox"
-        checked={selectAll}
-        onChange={e => handleSelectAll(e.target.checked)}
-        label="Select All" // Add this line to display "Select All" next to the checkbox
-      />
-    </th>
-            <th>Student Name</th>
-            <th>Student Code</th>
-            <th>Birth</th>
-            <th>CCCD</th>
-            <th>Status</th>
-            <th>Action</th>
-          </tr>
-        </thead>
-        <tbody>
-          {students.map(student => (
-            <tr key={student.id}>
-              <td>
-                <Form.Check
-                  type="checkbox"
-                  checked={selectedStudents.includes(student.id)}
-                  onChange={e =>
-                    handleSelectStudent(student.id, e.target.checked)
-                  }
-                />
-              </td>
-              <td>{student.name}</td>
-              <td>{student.code}</td>
-              <td>{student.birth}</td>
-              <td>{student.CCCD}</td>
-              <td>
-                <Badge
-                  bg={student.active ? 'info' : 'danger'}
-                  style={{ cursor: 'pointer' }}
-                  onClick={() => toggleStatus(student.id)}
-                >
-                  {student.active ? 'Active' : 'Inactive'}
-                </Badge>
-              </td>
-              <td>
-                <Button variant="info" onClick={() => handleEditStudent(student)}>
-                  Edit
-                </Button>{' '}
-                <Button
-                  variant="danger"
-                  onClick={() => handleDeleteStudent(student.id)}
-                >
-                  Delete
-                </Button>
-              </td>
-            </tr>
-          ))}
-        </tbody>
-      </Table>
+  <thead>
+    <tr>
+      <th colSpan={7}>
+        <Row className="align-items-center">
+          <Col xs="auto">
+            <Form.Check
+              type="checkbox"
+              checked={selectAll}
+              onChange={e => handleSelectAll(e.target.checked)}
+              label={`Selected Students: ${selectedStudents.length}`}
+            />
+          </Col>
+          <Col xs="auto">
+            <Button variant="dark" onClick={handleClearAll}>
+              Clear All
+            </Button>
+          </Col>
+        </Row>
+      </th>
+    </tr>
+    <tr>
+      <th></th>
+      <th>Student Name</th>
+      <th>Student Code</th>
+      <th>Birth</th>
+      <th>CCCD</th>
+      <th>Status</th>
+      <th>Action</th>
+    </tr>
+  </thead>
+  <tbody>
+    {students.map(student => (
+      <tr key={student.id}>
+        <td>
+          <Form.Check
+            type="checkbox"
+            checked={selectedStudents.includes(student.id)}
+            onChange={e =>
+              handleSelectStudent(student.id, e.target.checked)
+            }
+          />
+        </td>
+        <td>{student.name}</td>
+        <td>{student.code}</td>
+        <td>{student.birth}</td>
+        <td>{student.CCCD}</td>
+        <td>
+          <Badge
+            bg={student.active ? 'info' : 'danger'}
+            style={{ cursor: 'pointer' }}
+            onClick={() => toggleStatus(student.id)}
+          >
+            {student.active ? 'Active' : 'Inactive'}
+          </Badge>
+        </td>
+        <td>
+          <Button variant="info" onClick={() => handleEditStudent(student)}>
+            Edit
+          </Button>{' '}
+          <Button
+            variant="danger"
+            onClick={() => handleDeleteStudent(student.id)}
+          >
+            Delete
+          </Button>
+        </td>
+      </tr>
+    ))}
+  </tbody>
+</Table>
+
 
       {/* Modal for editing student information */}
-      <Modal show={showModal} onHide={() => setShowModal(false)}>
+      <Modal show={showEditModal} onHide={handleCloseEditModal}>
         <Modal.Header closeButton>
           <Modal.Title>Edit Student</Modal.Title>
         </Modal.Header>
@@ -256,7 +306,7 @@ function handleSelectStudent(id, isChecked) {
           </Form>
         </Modal.Body>
         <Modal.Footer>
-          <Button variant="secondary" onClick={() => setShowModal(false)}>
+          <Button variant="secondary" onClick={handleCloseEditModal}>
             Close
           </Button>
           <Button variant="dark" onClick={handleUpdateStudent}>
@@ -264,6 +314,63 @@ function handleSelectStudent(id, isChecked) {
           </Button>
         </Modal.Footer>
       </Modal>
+
+
+      {/* Modal for add student */}
+      <Modal show={showAddModal} onHide={handleCloseAddModal}>
+        <Modal.Header closeButton>
+          <Modal.Title>Add Student</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Form>
+            <Form.Group controlId="formName">
+              <Form.Label>Student Name</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Student Name"
+                value={name}
+                onChange={e => setName(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="formCode">
+              <Form.Label>Student Code</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Student Code"
+                value={code}
+                onChange={e => setCode(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="formBirth">
+              <Form.Label>Birth Date</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="Student Birth"
+                value={birth}
+                onChange={e => setBirth(e.target.value)}
+              />
+            </Form.Group>
+            <Form.Group controlId="formCCCD">
+              <Form.Label>CCCD</Form.Label>
+              <Form.Control
+                type="text"
+                placeholder="CCCD"
+                value={CCCD}
+                onChange={e => setCCCD(e.target.value)}
+              />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="secondary" onClick={handleCloseAddModal}>
+            Close
+          </Button>
+          <Button variant="dark" onClick={handleAddStudent}>
+            Add
+          </Button>
+        </Modal.Footer>
+      </Modal>
+      <h3>Total Student: {students.length}</h3>
     </div>
   );
 }
